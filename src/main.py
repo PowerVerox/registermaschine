9a310cd6486ae2e74aad346866262186910fc46c
Vincent Hey <vincent.hey@weidmueller.com>
2024-08-05 00:04:46 +0200

    able to customize prompt message, pythonize while input

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from result import Result, Ok, Err
 from instruction import *
 
-inp = input("> ")
-while inp.upper() != "X":
+prompt = "> "
+
+
+while (inp := input(prompt)).upper() != "X":
     inst = Instruction.from_string(inp)
     match inst:
         case Ok(value):
             print(value)
         case Err(e):
             print(e)
-    inp = input("> ")
