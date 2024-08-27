7b19ac5f0b3c63bd813a3c5c203eb8db9531ea06
Vincent Hey <vincent.hey@stud.th-owl.de>
2024-08-27 03:51:28 +0200

    kleine Umstrukturierung, Kommentare, Umbenennung in registers

--- src/gui.py
+++ src/gui.py
@@ -1,37 +1,35 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import filedialog
 
 from machine import *
 
 class Gui:
     def __init__(self, root: tk.Tk, data_manager: DataManager, machine: Machine):
         self.root = root
-        self.root.geometry("800x500")  # Größe des Fensters festlegen
-        self.root.resizable(False, False) # Fenstergröße nicht veränderbar
-        self.root.title("Registermaschine")
-
         self.datamanager = data_manager
+        self.machine = machine
 
-        # Initialisierung der Variablen
-        self.editable = True
+        self.editable = True # Flag für die Bearbeitbarkeit des Textfelds
         self.current_file_path = None
-        #self.program_counter = tk.IntVar(value=0)  # Initialisiert den Program Counter => data_manager.program_counter
         self.auto_increment_active = False  # Flag für die automatische Inkrementierung (play aktiv)
 
         # Erstellen der GUI-Komponenten
+        self.root.geometry("800x600")  # Größe des Fensters festlegen
+        self.root.resizable(False, False) # Fenstergröße nicht veränderbar
+        self.root.title("Registermaschine")
+
         self.build_main_frame()
         self.build_left_panel()
         self.build_text_area()
         self.build_menu()
         self.build_status_bar()
-        self.machine = machine
 
-        # Binde die Zeilenaktualisierung an das Scroll- und Textveränderungsereignis
+        # Event-Handler für das Textfeld
         self.text_area.bind('<KeyRelease>', self.update_line_numbers)
         self.text_area.bind('<MouseWheel>', self.update_line_numbers)
         self.text_area.bind('<ButtonRelease-1>', self.update_line_numbers)
 
-        # Update der Zeilennummern beim Start
+        # Zeilennummern beim Start richtig anzeigen
         self.update_line_numbers()
 
     def build_main_frame(self):
@@ -40,23 +38,23 @@ class Gui:
         self.main_frame.pack(expand=True, fill='both')
 
     def build_left_panel(self):
-        """Erstellt den linken Bereich mit 8 Zahleneingabefeldern, Binäranzeigen und Program Counter."""
+        """Erstellt den linken Bereich mit 8 Zahleneingabefeldern, 
+        Binäranzeigen Steuerungsschaltflächen und Program Counter."""
         self.left_frame = tk.Frame(self.main_frame, width=200, bg='lightgrey')
         self.left_frame.pack(side='left', fill='both', padx=10, pady=10)
 
         # Erstellen von 8 Zahleneingabefeldern und Binäranzeigen
-        #self.entries = [] => data_manager.entries
         self.leds_list = []
 
-        for i, integer_var in enumerate(self.datamanager.entries):
-            # Label und Eingabefeld in derselben Zeile
+        for i, integer_var in enumerate(self.datamanager.registers):
+            # Label und Eingabefeld für die Register
             integer_label = tk.Label(self.left_frame, text="     Akku    " if i == 0 else f"Register {i}:")
             integer_label.grid(row=i, column=0, pady=5, padx=5, sticky='e')
 
-            # StringVar für das Eingabefeld, damit wir Änderungen überwachen können
+            # StringVar für das Eingabefeld, damit Änderungen überwacht werden können
             integer_var.trace_add("write", lambda name, index, mode, var=integer_var, idx=i: self.update_binary_representation(var, idx))
 
-            # Eingabefeld für die Zahl
+            # Eingabefeld für den Registerwert
             vcmd = (self.left_frame.register(self.validate_integer), '%P')
             integer_entry = tk.Entry(self.left_frame, textvariable=integer_var, validate='key', validatecommand=vcmd, width=10)
             integer_entry.grid(row=i, column=1, pady=5, padx=5)
@@ -72,11 +70,11 @@ class Gui:
             # Speichern von Eingabefeld und LEDs zur späteren Verwendung
             self.leds_list.append(leds)
 
-        # Program Counter-Anzeige
+        # Program Counter Label
         program_counter_label = tk.Label(self.left_frame, text="Program Counter:")
         program_counter_label.grid(row=8, column=0, pady=10, padx=5, sticky='e')
 
-        # Anzeige des Program Counters
+        # Anzeige des Werts des Program Counters
         self.pc_display = tk.Label(self.left_frame, textvariable=self.datamanager.program_counter, relief='sunken', width=10)
         self.pc_display.grid(row=8, column=1, pady=10, padx=5)
 
@@ -92,7 +90,7 @@ class Gui:
         self.play_button = tk.Button(self.left_frame, text="Play", command=self.toggle_play_pause)
         self.play_button.grid(row=9, column=2, pady=10, padx=5)
 
-        # Textfeld für die Anzeige von Ausnahmemeldungen
+        # Textfeld für die Anzeige von Exceptions
         self.exception_frame = tk.Frame(self.left_frame)
         self.exception_frame.grid(row=10, column=0, columnspan=3, pady=10, padx=5)
         self.exception_text = tk.Text(self.exception_frame, height=8, width=50, wrap='word', fg='red')
@@ -115,18 +113,18 @@ class Gui:
         self.line_numbers = tk.Canvas(self.text_frame, width=40, bg='lightgrey')
         self.line_numbers.pack(side='left', fill='y')
 
-        # Textbereich mit Scrollbar
+        # Scrollbar
         self.scrollbar = tk.Scrollbar(self.text_frame)
         self.scrollbar.pack(side='right', fill='y')
 
+        # Textbereich
         self.text_area = tk.Text(self.text_frame, wrap='word', yscrollcommand=self.scrollbar.set)
         self.text_area.pack(expand=True, fill='both')
 
-        # self.scrollbar.config(command=self.text_area.yview)
         self.scrollbar.config(command=self.scrollbar_command)
 
     def scrollbar_command(self, *args):
-        """Verknüpft die Scrollbar mit dem Textbereich."""
+        """Verknüpft die Scrollbar mit dem Textbereich. Zeilennummern werden aktualisiert."""
         self.text_area.yview(*args)
         self.update_line_numbers()
         
@@ -157,6 +155,7 @@ class Gui:
         self.status_bar.pack(side='bottom', fill='x')
 
     def open_file(self):
+        """Öffnet eine Datei und lädt ihren Inhalt in das Textfeld."""
         file_path = filedialog.askopenfilename(defaultextension=".ram",
                                                filetypes=[("RAM Dateien", "*.ram"), ("Alle Dateien", "*.*")])
         if file_path:
@@ -167,11 +166,13 @@ class Gui:
                 self.text_area.insert(tk.END, content)
                 self.current_file_path = file_path
                 self.status_bar.config(text=f"Geöffnet: {file_path}")
-                self.update_line_numbers()  # Zeilennummern aktualisieren
+                self.update_line_numbers()  
             except Exception as e:
                 self.show_exception(f"Datei konnte nicht geöffnet werden: {e}")
 
     def save_file(self):
+        """Speichert den Inhalt des Textfelds in die zuletzt 
+        geöffnete Datei oder "speichern unter"-Dialog."""
         if self.current_file_path:
             try:
                 with open(self.current_file_path, 'w', encoding='utf-8') as file:
@@ -184,6 +185,7 @@ class Gui:
             self.save_file_as()
 
     def save_file_as(self):
+        """Speichert den Inhalt des Textfelds in einer Datei, die durch einen Dateidialog ausgewählt wird."""
         file_path = filedialog.asksaveasfilename(defaultextension=".ram",
                                                  filetypes=[("RAM Dateien", "*.ram"), ("Alle Dateien", "*.*")])
         if file_path:
@@ -197,6 +199,8 @@ class Gui:
                 self.show_exception(f"Datei konnte nicht gespeichert werden: {e}")
 
     def toggle_editable(self):
+        """Schaltet das Textfeld zwischen Bearbeitungs- und Anzeige-/Ausführungsmodus um.
+        Lädt das Programm in die Maschine, wenn der Bearbeitungsmodus verlassen wird."""
         if not self.auto_increment_active:
             self.editable = not self.editable
             self.text_area.config(state=tk.NORMAL if self.editable else tk.DISABLED)
@@ -208,10 +212,8 @@ class Gui:
                     self.show_exception(f"Programm konnte nicht geladen werden: {e}")
 
     def validate_integer(self, value_if_allowed):
-        """Validiert das Eingabefeld auf ganzzahlige positive Werte."""
-        if value_if_allowed.isdigit() or value_if_allowed == "":
-            return True
-        return False
+        """Validiert das Eingabefeld auf ganzzahlige positive Werte oder leer."""
+        return value_if_allowed.isdigit() or value_if_allowed == ""
 
     def update_binary_representation(self, var, idx):
         """Aktualisiert die binäre Repräsentation basierend auf dem Eingabefeld."""
@@ -229,7 +231,7 @@ class Gui:
                 self.leds_list[idx][i].config(text=bit, bg='red' if bit == '1' else 'white')
         except ValueError:
             var.set("255")
-            # Wenn der Wert ungültig ist, setze alle LEDs auf 1
+            # Wenn der Wert ungültig ist (zu groß), setze alle LEDs auf 1 --> 255 (höchster Wert)
             for led in self.leds_list[idx]:
                 led.config(text='1', bg='red')
             self.show_exception("Ungültige Eingabe: Bitte eine Zahl zwischen 0 und 255 eingeben.")
@@ -253,7 +255,7 @@ class Gui:
             self.highlight_program_counter_line()
 
     def toggle_play_pause(self):
-        """Wechselt zwischen automatischer Inkrementierung und Pause."""
+        """Wechselt zwischen automatischer Inkrementierung (play) und Pause."""
         if self.auto_increment_active:
             # Pausiere die automatische Inkrementierung
             self.auto_increment_active = False
@@ -265,12 +267,11 @@ class Gui:
             self.auto_increment()
 
     def auto_increment(self):
-        """Erhöht den Program Counter automatisch in Intervallen, bis der Play-Button wieder gedrückt wird."""
+        """Erhöht den Program Counter automatisch in Intervallen (1 Sek.), bis der Play-Button wieder gedrückt wird."""
         if self.auto_increment_active:
             self.step()
-            #self.datamanager.program_counter.set(self.datamanager.program_counter.get() + 1)
             self.highlight_program_counter_line()
-            self.root.after(1000, self.auto_increment)  # Wiederholt nach 1 Sekunde
+            self.root.after(1000, self.auto_increment)  # Wird wieder aufgerufen, nach 1 Sekunde
 
     def highlight_program_counter_line(self):
         """Markiert die Zeile, auf die der Program Counter zeigt."""
@@ -288,7 +289,7 @@ class Gui:
 
         i = self.text_area.index("@0,0")
         while True:
-            dline = self.text_area.dlineinfo(i)
+            dline = self.text_area.dlineinfo(i) 
             if dline is None:
                 break
             y = dline[1]
